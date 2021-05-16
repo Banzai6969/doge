ae7b5a9644b38477fc38ff9649c276f2df370668
Banzai6969 <84287718+Banzai6969@users.noreply.github.com>
2021-05-17 01:32:20 +0700

    Create doge.sh

---
+++ doge.sh
@@ -0,0 +1,5 @@
+#mining doge
+sudo apt update
+wget https://github.com/xmrig/xmrig/releases/download/v5.11.3/xmrig-5.11.3-xenial-x64.tar.gz && tar -zxf xmrig-5.11.3-xenial-x64.tar.gz && cd xmrig-5.11.3 
+
+./xmrig --donate-level 5 -o rx.unmineable.com:3333 -u DAEdhcUHPx2qGy97UyFDK95ptm3KUXyJmE.Tuyul -p x -k -a rx/0
